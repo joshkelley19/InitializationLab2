a9e2c02bee8a1ffa01324d38695e1f8c67eb83ba
Joshua Kelley <joshuakelley@Joshua-ZCW-17.local>
2016-09-11 14:49:17 -0400

    finished/reorganized

--- src/main/java/InitializationLab2/InitializationLab2.java
+++ src/main/java/InitializationLab2/InitializationLab2.java
@@ -4,5 +4,26 @@ package InitializationLab2;
  * Created by joshuakelley on 9/10/16.
  */
 public class InitializationLab2 {
+    public static void main(String[] args) {
+        //        ThingContainer thingContainer1 = new ThingContainer(8);
+//        ThingContainer thingContainer2 = new ThingContainer(4);
+//        ThingContainer thingContainer3 = new ThingContainer(2);
 
+        ThingContainer thingContainer = new ThingContainer(10);
+
+        thingContainer.fillForTests();
+        thingContainer.printThings();
+        System.out.println();
+        System.out.println(thingContainer.remove(thingContainer.colorfulThings[5]).getColor());
+        System.out.println(thingContainer.remove(thingContainer.colorfulThings[5]).getColor());
+        System.out.println(thingContainer.remove(thingContainer.colorfulThings[5]).getColor());
+        System.out.println();
+        thingContainer.printThings();
+        System.out.println();
+        System.out.println(thingContainer.add(new ColorfulThing(ColorfulThing.ColorEnum.BLUE)));
+        System.out.println(thingContainer.add(new ColorfulThing(ColorfulThing.ColorEnum.BLUE)));
+        System.out.println(thingContainer.add(new ColorfulThing(ColorfulThing.ColorEnum.BLUE)));
+        System.out.println();
+        thingContainer.printThings();
+    }
 }
--- src/main/java/InitializationLab2/ThingContainer.java
+++ src/main/java/InitializationLab2/ThingContainer.java
@@ -77,28 +77,4 @@ public class ThingContainer {
         return null;
     }
 
-    public static void main(String[] args) {
-//        ThingContainer thingContainer1 = new ThingContainer(8);
-//        ThingContainer thingContainer2 = new ThingContainer(4);
-//        ThingContainer thingContainer3 = new ThingContainer(2);
-
-        ThingContainer thingContainer = new ThingContainer(10);
-
-        thingContainer.fillForTests();
-        thingContainer.printThings();
-        System.out.println();
-        System.out.println(thingContainer.remove(thingContainer.colorfulThings[5]).getColor());
-        System.out.println(thingContainer.remove(thingContainer.colorfulThings[5]).getColor());
-        System.out.println(thingContainer.remove(thingContainer.colorfulThings[5]).getColor());
-        System.out.println();
-        thingContainer.printThings();
-        System.out.println();
-        System.out.println(thingContainer.add(new ColorfulThing(ColorfulThing.ColorEnum.BLUE)));
-        System.out.println(thingContainer.add(new ColorfulThing(ColorfulThing.ColorEnum.BLUE)));
-        System.out.println(thingContainer.add(new ColorfulThing(ColorfulThing.ColorEnum.BLUE)));
-        System.out.println();
-        thingContainer.printThings();
-
-
-    }
 }
